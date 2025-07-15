f91f4e2e9f5e5b51103d4bbbab170bcc449a82cd
Eulier González <eulier1@gmail.com>
2025-07-15 18:11:02 -0300

    add content structure and eng transl

--- app/[locale]/introduction/page.tsx
+++ app/[locale]/introduction/page.tsx
@@ -1,5 +1,13 @@
-import { setRequestLocale } from "next-intl/server";
+import { setRequestLocale, getTranslations } from "next-intl/server";
 import { routing } from "@/src/i18n/routing";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
@@ -11,17 +19,56 @@ export default async function Introduction({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
-
   // Enable static rendering for this locale
   setRequestLocale(locale);
+
+  const t = await getTranslations("Content.module1");
+
+  const listItems = t.raw("section1.descriptionList.list") as Array<{
+    id: number;
+    description: string;
+  }>;
+
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-      </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    <div className="flex flex-1 flex-col gap-4 bg-muted/50">
+      <Card className="h-full m-4">
+        <CardHeader>
+          <CardTitle>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
+              {t("section1.title")}
+            </h1>
+          </CardTitle>
+          <CardDescription>{t("section1.description")}</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-6">
+          <div>
+            <p>{t("section1.descriptionList.title")}</p>
+            <ul className="mt-2 ml-4 space-y-1">
+              {listItems.map((item) => (
+                <li key={item.id} className="list-disc">
+                  {item.description}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="space-y-4">
+            <div>
+              <h3 className="text-lg font-semibold mb-2">
+                {t("section2.title")}
+              </h3>
+              {t("section2.description")}
+            </div>
+
+            <div>
+              <h3 className="text-lg font-semibold mb-2">
+                {t("section3.title")}
+              </h3>
+              {t("section3.description")}
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
